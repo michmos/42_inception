77214c85671847800d61b7a9cb143b4bee384a53
Michael Moser <Michael.Moser11@web.de>
2025-06-24 22:15:43 +0200

    wordpress config: fix syntax issues

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -8,10 +8,10 @@
 
 
 /* MySQL settings */
-define( 'DB_NAME',     getenv('MYSQL_DATABASE') ?: 'database_name_here' );
-define( 'DB_USER',     getenv('MYSQL_USER') ?: 'username_here' );
-define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') ?: 'password_here' );
-define( 'DB_HOST',     getenv('MYSQL_HOST') ?: 'localhost' ); #database server address
+define( 'DB_NAME',     getenv('MYSQL_DATABASE'));
+define( 'DB_USER',     getenv('MYSQL_USER'));
+define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD'));
+define( 'DB_HOST',     getenv('MYSQL_HOST')); #database server address
 define( 'DB_CHARSET',  'utf8mb4' );
 
 
@@ -21,14 +21,14 @@ $table_prefix = 'wp_';
 
 /* Authentication Unique Keys and Salts. */
 /* https://api.wordpress.org/secret-key/1.1/salt/ */
-define('AUTH_KEY', getenv('AUTH_KEY');
-define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY');
-define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY');
-define('NONCE_KEY', getenv('NONCE_KEY');
-define('AUTH_SALT', getenv('AUTH_SALT');
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT');
-define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT');
-define('NONCE_SALT', getenv('NONCE_SALT');
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
 
 /* Absolute path to the WordPress directory. */
