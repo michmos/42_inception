5b675b355f47b06b48ceb5956fe727f1fd15b2ec
Michael Moser <Michael.Moser11@web.de>
2025-06-29 20:30:04 +0200

    add comment

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	# create wp-config.php
 	wp config create --path=/var/www/html/ --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST --dbprefix=wp_ --skip-check
-	# set filesystem method to direct #TODO: allows what?
+	# set filesystem method to allow php writing to file system (e.g. for plugins and themes)
 	wp config set FS_METHOD direct --config-file=/var/www/html/wp-config.php
 	# install wordpress
 	wp core install --path=/var/www/html/ --url=$WEBSITE_URL --title=$WEBSITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
