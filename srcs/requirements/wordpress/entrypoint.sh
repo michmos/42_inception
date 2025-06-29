92d79756c8485d9d2360cb08abca85fdedf177e0
Michael Moser <Michael.Moser11@web.de>
2025-06-29 19:50:16 +0200

    wp: config set FS_METHOD
    fs-method

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 	# create wp-config.php
 	wp config create --path=/var/www/html/ --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST --dbprefix=wp_ --skip-check
+	# set filesystem method to direct #TODO: allows what?
+	wp config set FS_METHOD direct --config-file=/var/www/html/wp-config.php
 	# install wordpress
 	wp core install --path=/var/www/html/ --url=$WEBSITE_URL --title=$WEBSITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email
 fi
