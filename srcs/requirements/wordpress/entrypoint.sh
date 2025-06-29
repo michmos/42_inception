8120b792d96f7bc280e1fdc1760187e67f92cece
Michael Moser <Michael.Moser11@web.de>
2025-06-29 19:47:12 +0200

    wp: download wp in dockerfile

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -3,12 +3,6 @@
 set -e
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-	mkdir -p /var/www/html
-	chown wp_user:wp_group /var/www/html
-	chmod 755 /var/www/html
-
-	# download wordpress
-	php -d memory_limit=512M /usr/local/bin/wp core download --path=/var/www/html
 	# create wp-config.php
 	wp config create --path=/var/www/html/ --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST --dbprefix=wp_ --skip-check
 	# install wordpress
