16ad0c902a96f99e3e1d99b16dc41ea6ee2a3dab
Michael Moser <Michael.Moser11@web.de>
2025-07-04 16:18:57 +0200

    rmv potentially infinite loop
    not required anymore through startup based on health check

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -16,10 +16,6 @@ fi
 
 # install wordpress
 if [ ! -f "/var/www/html/.wp_initialized" ]; then
-	until mysql "$WP_DB" -h "$WP_DB_HOST" -u "$WP_DB_USER" --password="$WP_DB_PASSWORD" 2> /dev/null; do
-		echo "Waiting for mariadb to be available..."
-		sleep 2
-	done
 	wp core install --path=/var/www/html/ --url="$WP_WEBSITE_URL" --title="$WP_WEBSITE_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 	wp user create --path=/var/www/html/ "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD"
 	# mark successful installation
